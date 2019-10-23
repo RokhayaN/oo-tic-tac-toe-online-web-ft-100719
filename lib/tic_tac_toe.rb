c7af366308fd96225a1e43f429a3c01c4e07c3db
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-23 04:26:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,5 +14,6 @@ class TicTacToe
     puts "-----------"
     puts "#{@board[3]}|#{@board[4]}|#{@board[5]}"
     puts "-----------"
-    puts 
+    puts "#{@board[6]}|#{@board[7]}|#{@board[8]}"
 end
+

0fbc8ead9e70075a8d8039cfd6024ab3e8a2be19
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-23 04:21:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,6 +10,6 @@ class TicTacToe
     [0, 4, 8], [2, 4, 6]
   ]
   def display_board (board)
-    puts "#{@board[0]}|#{@board[1]}|#{@board[2]}
-    
+    puts "#{@board[0]}|#{@board[1]}|#{@board[2]}"
+    puts "________"
 end
